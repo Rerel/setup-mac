e8ea0e16020ce79168de981c01b3fe170c756cca
Aurélien Lemesle <aurelien@mentallyfriendly.com>
2015-08-04 13:46:35 +0100

    initial commit

---
+++ setup-mac.sh
@@ -0,0 +1,109 @@
+# Aurélien Lemesle
+# Install tool for mac
+# 4 Aug 2015
+
+# Check for Homebrew,
+# Install if we don't have it
+if test ! $(which brew); then
+  printf "\e[34m=> Installing: \033[0m Homebrew...\n"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update homebrew recipes
+brew update
+
+# Install GNU core utilities (those that come with OS X are outdated)
+brew install coreutils
+
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+brew install findutils
+
+# install common brew tools
+
+binaries=(
+    zsh
+    emacs
+    ack
+    cloc
+    git
+    htop-osx
+    tig
+    tree
+    openssl
+    wget
+    terminal-notifier
+    rbenv
+    heroku-toolbelt
+    xctool
+)
+
+echo “installing binaries…”
+brew install ${binaries[@]}
+
+# clean up
+brew cleanup
+
+# Install DotFiles
+printf "\e[34m=> Installing: \033[0m DotFiles (emacs, git, zsh)...\n"
+cd ~/Documents
+git clone git@github.com:Rerel/DotFiles.git
+cd DotFiles
+cp -r .emacs.d ~/.
+cp .gitconfig ~/.
+cp .gitignore_global ~/.
+cp .z* ~/.
+cd ..
+rm -rf DotFiles
+
+# Oh my Zsh install
+printf "\e[34m=> Installing: \033[0m Oh my Zsh...\n"
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+# Install Cask
+brew install caskroom/cask/brew-cask
+
+# Install Casks
+apps=(
+    google-chrome
+    alfred
+    transmission
+    spectacle
+    textexpander
+    backblaze
+    charles
+    dash
+    dropbox
+    laullon-gitx
+    handbrake
+    kaleidoscope
+    pd-extended
+    reveal
+    paintcode
+    spotify
+    atom
+    sublime-text
+    vlc
+    whatsize
+    duet
+    dashlane
+    slack
+    evernote
+    imageoptim
+    ios-console
+    sourcetree
+    the-unarchiver
+    limechat
+    xquartz
+    github
+)
+
+# Install apps to /Applications
+# Default is: /Users/$user/Applications
+printf "\e[34m=> Installing: \033[0m Homebrew cask recipes...\n"
+brew cask install --appdir="/Applications" ${apps[@]}
+
+brew tap caskroom/versions
+
+# Cocoapods install
+printf "\e[34m=> Installing: \033[0m Cocoapods...\n"
+sudo gem install cocoapods
